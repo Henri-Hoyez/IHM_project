7da3ab9a26bcd1177cd893928e60e5a0725c9d1c
Nicolas Thuylie <nicolas.thuylie@laposte.net>
2019-09-26 14:41:37 +0200

    Clean master

---
+++ src/components/textfield/my-textfield.js
@@ -0,0 +1,29 @@
+import { LitElement, html } from 'lit-element';
+import {MDCTextField} from '@material/textfield'
+
+
+class MyTextField extends LitElement{
+
+    constructor(){
+        super();
+    }
+
+    firstUpdated(){
+        MDCTextField.attachTo(this.shadowRoot.querySelector('.mdc-text-field'));
+    }
+
+    render(){
+        return html`
+            <link rel="stylesheet" href="node_modules/@material/textfield/dist/mdc.textfield.css">
+            <script src="node_modules/@material/textfield/dist/mdc.textfield.js"></script>
+
+            <div class="mdc-text-field">
+                <label class="mdc-floating-label" for="my-text-field"> <slot> Hint text </slot> </label>
+                <input type="text" id="my-text-field" class="mdc-text-field__input">
+            <div class="mdc-line-ripple"></div>
+        </div>`;
+    }
+
+}
+
+customElements.define("my-textfield", MyTextField);
---
+++ src/components/top_bar/topBar.js
@@ -0,0 +1,65 @@
+import {LitElement, css,html} from "lit-element";
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+
+class TopBar extends LitElement{
+    static get properties(){
+        return {
+
+        };
+    }
+    static get styles(){
+        return css`:host
+            .material-icons {
+                font-family: 'Material Icons';
+                font-weight: normal;
+                font-style: normal;
+                font-size: 24px;
+                display: inline-block;
+                line-height: 1;
+                text-transform: none;
+                letter-spacing: normal;
+                word-wrap: normal;
+                white-space: nowrap;
+                direction: ltr;
+            }
+        `
+    }
+    constructor(){
+        super();
+    }
+    firstUpdated(){
+        var topAppBarElement = this.shadowRoot.querySelector(".mdc-top-app-bar");
+        console.log(topAppBarElement);
+        const topAppBar = new MDCTopAppBar(topAppBarElement);
+    }
+    render(){
+        return html`
+        <style>
+            @import 'mdc.top-app-bar.css';
+            @import 'mdc.icon-button.css';
+            @import 'top-bar.css';
+        </style>
+        
+        <header class="mdc-top-app-bar  mdc-top-app-bar--prominent">
+            <div class="mdc-top-app-bar__row">
+                <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                    <button class="mdc-icon-button material-icons mdc-top-app-bar__navigation-icon--unbounded">menu</button>
+                     
+                </section>
+                <div id= "icon-header">
+                    <a id="icon-header-title" href="#">
+                        <img id="icon-header-img" src="web_icon2.png">
+                        <span  class="mdc-top-app-bar__title">Little fox</span>
+                    </a>
+                </div>
+                <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+                    <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item--unbounded" aria-label="Profile">person</button>
+                    <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item--unbounded" aria-label="Shopping cart">shopping_cart</button>
+                </section>
+            </div>
+        </header>
+        `
+    }
+}
+customElements.define("top-bar",TopBar);
